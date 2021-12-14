fa8749c0a48245b29b3a997a06bf65196bbc849f
transparent412 <oleg.senderov@mail.ru>
2021-12-14 22:32:08 +0400

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
