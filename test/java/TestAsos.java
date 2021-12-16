17e7fb97fd055f38ffc09ddb49def0292c1c2696
transparent412 <oleg.senderov@mail.ru>
2021-12-16 19:52:44 +0400

    bug fixes

---
+++ test/java/TestAsos.java
@@ -0,0 +1,95 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TestAsos {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUpConnection() {
+
+        System.setProperty("webdriver.chrome.driver", "./resources/chromedriver.exe");
+
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://www.asos.com/ru/");
+
+    }
+
+    @Test
+    public void testTool() {
+
+        Actions actions = new Actions(driver);
+
+        WebElement element = (new WebDriverWait(driver, Duration.ofSeconds(20))).until(ExpectedConditions
+                .presenceOfElementLocated(
+                        By.xpath("//button[@class='src-GlobalBanner-Content-Content_content src-GlobalBanner-Content-Content_contentCenter']")));
+        actions.moveToElement(element).perform();
+        Assert.assertEquals("*Введи промокод HEYYOU на странице оплаты заказа, чтобы получить скидку. " +
+                "Акция заканчивается в 09:00 МСК 24.01.2022. Промокод можно использовать только при первом заказе. " +
+                "Промокод не может быть использован совместно с другими предложениями и кодами, а также для покупки подарочных сертификатов, для оплаты доставки и для покупок на ASOS Marketplace. " +
+                "Действует только на заказы с доставкой в Россию, Казахстан и Кыргызстан. Некоторые отмеченные товары не участвуют в акции.", driver.findElement(By.xpath("//*[@id='globalBannerComponent']/div/div/div/div")).getText());
+    }
+
+    @Test
+    public void testSort() {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        driver.findElement(By.xpath("//a[@class='mu__cta']/span[text()='МУЖСКОЕ']")).click();
+        driver.findElement(By.xpath("//a[@data-analytics-id='ru-mwgblexcau-winteraccessories-mulink']")).click();
+
+        driver.findElement(By.xpath("//li[@data-dropdown-id='attribute_1047']/div/button/div[text()='Тип продукта']")).click();
+        driver.findElement(By.xpath("//li[@class='_3LB03xF']/div/label/div[text()='Перчатки']")).click();
+
+        driver.findElement(By.xpath("//li[@data-auto-id='sort']")).click();
+        driver.findElement(By.xpath("//li[@id='plp_web_sort_price_low_to_high']")).click();
+
+        List<WebElement> list = driver.findElements(By.xpath("//section[@class='_3YREj-P']/article/a/p/span/span"));
+
+        List<String> list2 = list.stream().map(WebElement::getText).collect(Collectors.toList());
+        list2 = list2.stream().map(s -> s.substring(0, s.indexOf(","))).collect(Collectors.toList());
+
+        List<Integer> sortedList = new ArrayList<>(list2.size());
+        list2.forEach(i -> sortedList.add(Integer.parseInt(i.replaceAll(" ", ""))));
+
+        boolean flag = IntStream.range(0, sortedList.size() - 1).allMatch(i -> sortedList.get(i) <= sortedList.get(i + 1));
+
+        Assert.assertTrue(flag, "Сортировка не отлично, сортировка не all right");
+
+    }
+
+    @Test
+    public void invalidRegistrationTest() {
+
+        driver.findElement(By.xpath("//button[@data-testid='myAccountIcon']")).click();
+        driver.findElement(By.xpath("//a[@data-testid='signup-link']")).click();
+
+        driver.findElement(By.xpath("//input[@class='qa-email-textbox']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@class='qa-firstname-textbox']")).sendKeys("Олег");
+        driver.findElement(By.xpath("//input[@class='qa-lastname-textbox']")).sendKeys("Сендеров");
+        driver.findElement(By.xpath("//input[@class='qa-password-textbox']")).sendKeys("qwerty123456");
+        driver.findElement(By.xpath("//select[@class='qa-birthday-textbox']/option[@value='1']")).click();
+        driver.findElement(By.xpath("//select[@class='qa-birthmonth-textbox']/option[@value='3']")).click();
+        driver.findElement(By.xpath("//select[@class='qa-birthyear-textbox']/option[@value='2005']")).click();
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
+
+        String message = driver.findElement(By.xpath("//*[@id='Email-error']")).getText();
+        Assert.assertTrue(message.isEmpty(), "Пустое обязательное поле Email");
+
+    }
+
+}
